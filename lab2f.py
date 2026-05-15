f91391bffd8e376148bd69c62ab0f1666253975f
Thomas Chu <tchu20@myseneca.ca>
2026-05-15 18:54:05 -0400

    Added lab2f.

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author: Thomas Chu
+#Author ID: tchu20
+#Date Created:2026/05/15
+
+import sys
+
+int(sys.argv[1])
+
+count = int(sys.argv[1])
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
