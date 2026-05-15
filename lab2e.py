bab3507b364935a9195b25a28f1fdcf83c423efd
Thomas Chu <tchu20@myseneca.ca>
2026-05-15 18:53:37 -0400

    Added lab2e.

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Author: Thomas Chu
+#Author ID: tchu20
+#Date Created:2026/05/15
+
+
+count = 10
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
